9679d7624551a1394bde09e1bfa508c2e0e18b00
Frvnki <frankiejdaniels@gmail.com>
2022-03-11 16:08:27 -0800

    error correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
  *  * main - prints the alphabet in lowercase, and then in uppercase,
  *   * followed by a new line
  *    * Return: Always 0 (Success)
- *     */
+      */
 int main(void)
 {
 	int ch;
@@ -13,6 +13,6 @@ int main(void)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
