7d82d3bc5d4097ebd9059fd80efe724aa1940c7f
Frvnki <frankiejdaniels@gmail.com>
2022-03-16 18:46:51 -0700

    error correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
-/*
- * print_alphabet  - prints the alphabet, in lowercase
+/**
+ * print_alphabet - prints the alphabet, in lowercase
  * Return: Always 0 (Succes)
  */
+
 void print_alphabet(void)
 {
 	char ch;
