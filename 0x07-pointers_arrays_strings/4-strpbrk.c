2ac4fa86dbf34373f3e73ffdda6232203812af14
Frvnki <frankiejdaniels@gmail.com>
2022-04-03 08:27:55 -0700

    error correction

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,9 +6,9 @@
  *   * @s: string to search
  *    * @accept: stringcontaining the bytes to look for
  *     *
- *      * Return: pointer to the byte in s that matches one of the bytes in accept
+ *Return: pointer to the byte in s that matches one of the bytes in accept
  *       * or NULL if no such byte is found
- *        */
+  */
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
